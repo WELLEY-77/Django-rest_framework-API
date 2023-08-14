49ec1062ef34fa175d68a31247a0087632a77cb7
Welley <monteirowelley7@gmail.com>
2023-08-14 14:20:57 -0300

    criando o models, e modificando o admin

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+class ClientesList(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'celular')
+    list_display_links = ('nome', 'email', 'celular')
+    list_editable = ('ativo',)
+    list_filter = ('ativo',)
+    list_per_page = 10
+    search_fields = 'nome'
--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Clientes(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail', blank=False, max_length=30)
+    cpf = models.CharField('CPF', max_length=11, unique=True)
+    rg = models.CharField('RG', max_length=9)
+    celular = models.CharField('Celular', max_length=14)
+    ativo = models.BooleanField()
+
+    def __str__(self):
+        return self.nome
