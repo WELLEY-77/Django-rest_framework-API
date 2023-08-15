2c486228a46c2afa1e9ced9c64266608647550fa
Welley <monteirowelley7@gmail.com>
2023-08-15 17:47:42 -0300

    criando um campo de busca e filtrando por nome, e cpf

--- clientes/admin.py
+++ clientes/admin.py
@@ -4,7 +4,7 @@ from clientes.models import Cliente
 class ClienteList(admin.ModelAdmin):
     list_display = ('id','nome', 'email', 'cpf', 'celular','ativo')
     list_display_links = ('id','nome', 'email', 'celular')
-    search_fields = ('nome',)
+    search_fields = ('nome','cpf')
     list_editable = ('ativo',)
     list_filter = ('ativo',)
     list_per_page = 10
--- clientes/views.py
+++ clientes/views.py
@@ -9,5 +9,7 @@ class ClientesViewSet(viewsets.ModelViewSet):
 
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer 
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome',]
+    search_fields = ['nome', 'cpf']
+    filterset_fields = ['ativo',]
